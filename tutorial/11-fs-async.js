3563d00bc106db18f00888055a3b682e2aa1b0ea
Francisco Farinha <fquico1999@gmail.com>
2022-07-05 22:08:44 -0700

    Sample usage of fs async module

---
+++ tutorial/11-fs-async.js
@@ -0,0 +1,30 @@
+const {readFile, writeFile, read} = require('fs')
+
+
+// If we don't provide encoding, it will return a Buffer
+readFile('./content/first.txt','utf8' ,(err, res)=>{
+    if (err){
+        console.log(err)
+        return
+    }
+    console.log(res)
+
+    const first = res 
+
+    readFile('./content/second.txt', 'utf8', (err, res)=>{
+        if (err){
+            console.log(err)
+            return  
+        }
+        console.log(res) 
+
+        const second = res 
+
+        writeFile('./content/result-async.txt', `Here is the result: ${first}, ${second}`, (err)=>{
+            if (err){
+                console.log(err)
+            }
+        })
+    })
+    
+})
