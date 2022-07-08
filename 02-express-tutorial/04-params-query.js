011de79d87e977e1536f2cbc6272ee8b248b0ca4
Francisco Farinha <fquico1999@gmail.com>
2022-07-08 01:06:12 -0700

    Added example of using queries to filter served results.

--- 02-express-tutorial/04-params-query.js
+++ 02-express-tutorial/04-params-query.js
@@ -29,9 +29,52 @@ app.get('/products/:productID', (req, res)=>{
     }
 })
 
+app.get('/products/:productID/reviews/:reviewID', (req, res)=>{
+    console.log(req.params)
+    res.send('This is a dummy product review')
+})
+
+// The way query functions is that any key value pair included after a ? in the url
+// i.e /api/v1/query?key1=value1&key2=value2 will be made available in the req.query object
+// As key value pairs - {key1: 'value1', key2:  'value2'}
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+
+    // Set up query for search and limit
+    const {search, limit} = req.query
+    let sortedProducts = [...products] // Use let since we can potentially alter this list.
+
+    if (search){
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if (limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if (sortedProducts.length < 1 ){
+        //res.status(200).send('no products match your search')
+
+        // Ensure we use return here, otherwise express will move onto next line
+        // and have two returns for a single request.
+        return res.status(200).json({sucess: true, data:[]})
+    }
+    res.status(200).json(sortedProducts)
+})
+
 
 app.get('/people', (req, res)=>{
     res.json(people)
+})
+
+app.get('/people/:personID', (req, res)=>{
+    const person = people.find((person)=> person.id === Number(req.params.personID))
+
+    if (person){
+        res.send(`<h1>Hello ${person.name}</h1>`)
+    }else{
+        res.status(404).send("Person Not Found")
+    }
 })
 
 // Handle 404
