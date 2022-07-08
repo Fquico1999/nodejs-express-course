16c15ef3ff48196e797f9de5dfb935a149e5631f
Francisco Farinha <fquico1999@gmail.com>
2022-07-08 14:55:28 -0700

    Added mongodb connection with mongoose.

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
 // middleware
 app.use(express.json())
@@ -21,6 +22,16 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 
-app.listen(port, ()=>{
-    console.log(`Listening on Port ${port}...`)
-})
+// Use async since mongoose.connect returns promise
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, ()=>{
+            console.log(`Listening on Port ${port}...`)
+        })
+    }catch (error){
+        console.log(error)
+    }
+}
+
+start()
