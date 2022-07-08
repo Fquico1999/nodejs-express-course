26614b85686e9c8bbfbdf16d4f848af690bfe49f
Francisco Farinha <fquico1999@gmail.com>
2022-07-08 15:04:02 -0700

    Implemented createTask method in controllers.

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+// Only properties in the Schema will be created.
+// Any requests with more properties will be ignored.
 const TaskSchema = new mongoose.Schema({
     name:String,
     completed:Boolean
